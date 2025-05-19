3115aedd73804a5505f88a047281fc619aa7212d
Adrian Boström <bostromadrian@gmail.com>
2025-05-19 15:53:01 +0200

    Tests runnar och allt

--- Seminar 4/kassa/src/test/java/se/kth/iv1350/model/SaleTest.java
+++ Seminar 4/kassa/src/test/java/se/kth/iv1350/model/SaleTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import se.kth.iv1350.DTO.ItemDTO;
 import se.kth.iv1350.model.Item;
 import se.kth.iv1350.model.Sale;
 
@@ -20,8 +21,9 @@ public class SaleTest {
 
     @Test
     public void testAddItem() {
-
-        Item newItem = new Item(100, 1, "TESTER", 10F, 0.1F, "TEST ITEM");
+        ItemDTO itemDTO = new ItemDTO(100, "TestName", 10F, 0.1F,
+                "TEST ITEM description");
+        Item newItem = new Item(itemDTO, 1);
         sale.addItem(newItem);
         Item foundItem = sale.findItem(100);
         assertTrue(foundItem != null, "Could not find added item");
@@ -30,42 +32,40 @@ public class SaleTest {
 
     @Test
     public void testAddDuplicateItem() {
-
-        Item newItem = new Item(102, 1, "YouGoGo Blueberry", 14.9F, 0.06F,
+        ItemDTO itemDTO = new ItemDTO(102, "YouGoGo Blueberry", 14.9F, 0.06F,
                 "YouGoGo Blueberry 240g, low sugar yoghurt, blueberry flavour.");
+        Item newItem = new Item(itemDTO, 1);
         sale.addItem(newItem);
         Item foundItem = sale.findItem(102);
         assertTrue(foundItem != null, "Could not find added duplicate item");
-
     }
 
     @Test
     public void testCalculateTotal() {
-
-        Item newItem = new Item(102, 1, "YouGoGo Blueberry", 14.9F, 0.06F,
+        ItemDTO itemDTO = new ItemDTO(102, "YouGoGo Blueberry", 14.9F, 0.06F,
                 "YouGoGo Blueberry 240g, low sugar yoghurt, blueberry flavour.");
+        Item newItem = new Item(itemDTO, 1);
         sale.addItem(newItem);
         sale.addItem(newItem);
         sale.calculateTotal();
         float total = sale.getTotalPrice();
         float expected = 15.794F;
         assertTrue(total == expected, "Expected price did not match calculated price");
-
     }
 
     @Test
     public void testCheckDuplicate() {
-
-        Item newItem = new Item(102, 1, "YouGoGo Blueberry", 14.9F, 0.06F,
+        ItemDTO itemDTO1 = new ItemDTO(102, "YouGoGo Blueberry", 14.9F, 0.06F,
                 "YouGoGo Blueberry 240g, low sugar yoghurt, blueberry flavour.");
+        Item newItem = new Item(itemDTO1, 1);
         sale.addItem(newItem);
-        newItem = new Item(101, 2, "BigWheel Oatmeal", 29.9F, 0.06F,
+        ItemDTO itemDTO2 = new ItemDTO(101, "BigWheel Oatmeal", 29.9F, 0.06F,
                 "BigWheel Oatmeal 500 g, whole grain oats, 7 high fiber, gluten free");
+        newItem = new Item(itemDTO2, 2);
         sale.addItem(newItem);
         int duplicate = sale.checkDuplicate(newItem);
         int expected = 1;
         assertTrue(duplicate == expected, "Duplicate was not found");
-
     }
 
 }
