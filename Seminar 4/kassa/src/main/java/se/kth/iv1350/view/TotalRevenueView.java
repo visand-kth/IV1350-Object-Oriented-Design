dddf26e08cd3dfef7edacf69d03b58ee12992c00
Viktor Sandström <visand@kth.se>
2025-05-28 11:24:04 +0200

    Delete all

--- Seminar 4/kassa/src/main/java/se/kth/iv1350/view/TotalRevenueView.java
+++
@@ -1,8 +0,0 @@
-package se.kth.iv1350.view;
-import se.kth.iv1350.view.TotalRevenueObserver;
-
-public class TotalRevenueView implements TotalRevenueObserver {
-    public void newSale(double revenue) {
-        System.out.println("Total revenue: " + revenue);
-    }
-}
